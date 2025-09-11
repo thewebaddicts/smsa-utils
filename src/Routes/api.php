51db2464113f07af4a8fa0efd44efc23237dccc1
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-09-11 12:19:33 +0300

    xx

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//  Route::prefix('api/v1')->middleware([twa\smsautils\Http\Middleware\AuthMandatoryMiddleware::class])->group(function () {
-Route::group(['prefix' => 'api/v1', 'middlewares' => [twa\smsautils\Http\Middleware\AuthMandatoryMiddleware::class]], function () {
+ Route::prefix('api/v1')->middleware([twa\smsautils\Http\Middleware\AuthMandatoryMiddleware::class])->group(function () {
     Route::post('/files', action: [twa\smsautils\Http\Controllers\FileController::class, 'upload']);
 });
