85458b7829a918879ae3a9499145b3af5cffa24b
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-09-11 19:22:22 +0300

    rr

---
+++ src/Models/Company.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace twa\smsautils\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'companies';
+    protected $fillable = ['label'];
+} 
