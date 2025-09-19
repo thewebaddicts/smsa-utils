de8a2199f9ba5cdfff63c59543984e292e51e2fe
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-09-19 17:24:27 +0300

    Update default.php

--- src/Helpers/default.php
+++ src/Helpers/default.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 
 if (!function_exists('log_activity')) {
@@ -28,7 +29,7 @@ if (!function_exists('log_activity')) {
                 $files ?? []
             );
         } catch (\Exception $e) {
-// dd($e);
+            // dd($e);
             Log::error('Failed to dispatch LogActivityJob: ' . $e->getMessage());
         }
     }
@@ -78,5 +79,77 @@ if (!function_exists('query_options_response')) {
                 'label' => $item->$columnLabel,
             ];
         });
+    }
+}
+
+
+
+if (!function_exists('get_currencies_rates')) {
+    function get_currencies_rates()
+    {
+
+        $currencies = Cache::get('currencies_rates');
+
+        if (!$currencies) {
+            $currencies = DB::table('currencies')->whereNull('deleted_at')->pluck('rate', 'iso')->toArray();
+            Cache::put('currencies_rates', $currencies, 86400);
+
+            return $currencies;
+        }
+
+        return $currencies;
+    }
+}
+
+
+if (!function_exists('currencies_conversion')) {
+    function currencies_conversion($amounts, $currency)
+    {
+        $total = 0;
+
+        foreach ($amounts as $amount) {
+            $total += convert_from_to($amount['amount'], $amount['currency'], $currency);
+        }
+
+        return $total;
+    }
+}
+
+
+if (!function_exists('convert_from_to')) {
+    function convert_from_to($amount, $from_currency, $to_currency)
+    {
+
+        if ($from_currency == $to_currency) {
+            return $amount;
+        }
+
+        $rates = get_currencies_rates();
+
+        if (!isset($rates[$amount['currency']])) {
+            return 0;
+        }
+
+        return ($amount * $rates[$to_currency]) / $rates[$from_currency];
+    }
+}
+
+
+if (!function_exists('send_courier_notification')) {
+    function send_courier_notification($courier_id, $code)
+    {
+
+        $notification_template = DB::table('notification_templates')->where('code', $code)->first();
+        if (!$notification_template) {
+            return false;
+        }
+
+        DB::table('courier_notifications')->insert([
+            'courier_id' => $courier_id,
+            'notification_template_id' => $notification_template->id,
+            'read_at' => null
+        ]);
+
+        return true;
     }
 }
