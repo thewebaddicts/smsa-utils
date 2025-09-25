cf81c897cafc7c2940b45a75acabf54e2f62df7f
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-09-25 19:11:11 +0300

    ff

---
+++ src/Enums/DimensionUnit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace twa\smsautils\Enums;
+
+enum DimensionUnit: string
+{
+    case CM = 'cm';
+    case M = 'm';
+    case MM = 'mm';
+    case IN = 'in';
+    case FT = 'ft';
+} 
---
+++ src/Enums/WeightUnit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace twa\smsautils\Enums;
+
+enum WeightUnit: string
+{
+    case G = 'g';
+    case KG = 'kg';
+    case LB = 'lb';
+    case LBS = 'lbs';
+} 
