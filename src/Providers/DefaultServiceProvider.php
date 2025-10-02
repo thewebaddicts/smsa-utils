ec5a727998b3c80930c78edcd10fa357f048d461
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2025-10-02 10:32:19 +0300

    Update DefaultServiceProvider.php

--- src/Providers/DefaultServiceProvider.php
+++ src/Providers/DefaultServiceProvider.php
@@ -4,14 +4,18 @@ namespace twa\smsautils\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Database\Eloquent\Relations\Relation;
 class DefaultServiceProvider extends ServiceProvider
 {
 
 
     public function boot()
     {
-
+        
+        Relation::enforceMorphMap([
+            'operator' => 'twa\smsautils\Models\Operator',
+            'courier' => 'twa\smsautils\Models\Courier',
+        ]);
         $this->publishes([
             __DIR__ . '/../Configs/smsa-utils.php' => config_path('smsa-utils.php'),
         ], 'smsa-utils-config');
