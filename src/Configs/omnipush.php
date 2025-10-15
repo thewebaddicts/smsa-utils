58eda177a8018cf8ee509524f67bbb235280b33d
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-10-15 15:04:39 +0300

    Create omnipush.php

---
+++ src/Configs/omnipush.php
@@ -0,0 +1,12 @@
+<?php
+ 
+ 
+return [
+    'onesignal' => [
+        "provider" => "onesignal",
+        "data" => [ "AppID" => "93707c2c-d57b-466f-9ca3-c581407d9f99", "AuthKey" => "os_v2_app_snyhylgvpndg7hfdywaua7m7tfqh5z3crjleu7veeeg64iyfkvvhhzq4hj3y474ugfynrjptlntl3e6v3octfdcfyuxjklry5s5uq4q" ],
+    ]
+];
+ 
+ 
+ 
