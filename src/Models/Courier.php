82179c3c27c69d9fb47f8368a2eb7ebe2ad2c27c
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2025-11-21 10:47:11 +0200

    Update Courier.php

--- src/Models/Courier.php
+++ src/Models/Courier.php
@@ -63,11 +63,64 @@ class Courier extends Model
 
     public function getEmployeeReferenceAttribute()
     {
-      return $this->courier_id;
+        return $this->courier_id;
     }
 
     public function getDisplayNameAttribute()
     {
         return $this->getFullNameAttribute();
+    }
+   
+    public function assignedVehicle()
+    {
+        return $this->belongsTo(\App\Models\Vehicle::class, 'assigned_vehicle')->with(['brand']);
+    }
+
+
+    public function format()
+    {
+        $vehicle = $this->assignedVehicle;
+
+        return [
+            'id' => $this->id,
+            'courier_id' => $this->courier_id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'display_name' => $this->display_name,
+            'phone_number' => $this->phone_number,
+            'email' => $this->email,
+            'national_id' => $this->national_id,
+            'address' => $this->address,
+            'driving_license_number' => $this->driving_license_number,
+            'license_expiry_date' => $this->license_expiry_date,
+            'license_type' => $this->license_type,
+            'hire_date' => $this->hire_date,
+            'status' => optional(\App\Enums\CourierStatusesEnum::tryFrom($this->status))->info(),
+            'assigned_vehicle' => $this->assigned_vehicle,
+            'vehicle' => $vehicle ? [
+                'id' => $vehicle->id,
+                'license_plate_number' => $vehicle->license_plate_number,
+                'model' => $vehicle->model,
+                'palette_capacity' => $vehicle->palette_capacity,
+                'year' => $vehicle->year_of_manufacture,
+                'brand' => $vehicle->brand ? [
+                    'id' => $vehicle->brand->id,
+                    'label' => $vehicle->brand->name
+                ] : null,
+            ] : null,
+            'type' => $this->type,
+            'company_id' => $this->company_id,
+            'company' => $this->company?->label,
+            'contact_person_name' => $this->contact_person_name,
+            'contact_phone_number' => $this->contact_phone_number,
+            'relationship' => $this->relationship,
+            'driving_license_scan' => $this->driving_license_scan,
+            'national_id_scan' => $this->national_id_scan,
+            'notes' => $this->notes,
+            'assigned_routes' => $this->assignedRoutes->map(fn($route) => [
+                'id' => $route->id,
+                'display_name' => $route->label,
+            ])->values()->toArray(),
+        ];
     }
 }
