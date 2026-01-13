d0d0506c6950f3db6e3d119361a3c823f382d139
avedis-abakelian <nourhane.sarieddine@thewebaddicts.com>
2026-01-13 12:57:14 +0200

    e

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Client extends Model
+class Account extends Model
 {
     use HasFactory, SoftDeletes;
     
--- src/Models/Shipment.php
+++ src/Models/Shipment.php
@@ -7,60 +7,59 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shipment extends Model
 {
-    use SoftDeletes;
+  use SoftDeletes;
 
 
   protected $fillable = [
 
-        'scheduled_date',
-        'reference1',
-        'reference2',
-        'reference3',
-        'payment_reference1',
-        'payment_reference2',
-        'payment_reference3',
-        'added_services',
-        'transaction_id',
-        'billing_reference',
-        'currency',
-        'product_code',
-        'product_group',
-        'payment_type',
-        'payment_method',
-        'sender_address_id',
-        'receiver_address_id',
-        'client_id',
-        'customer_id',
-        'customs_payment_type',
-         'vat_payment_type',
-        'freight_payment_method',
-        'customs_payment_method',
-        'vat_payment_method',
-        'freight_payment_type',
-        'insurance',
-        'freight_payment_type',
-        'sla_delivery_date',
-        'dutiable',
-          'source',
-        'created_at',
-        // ROZANA
-        'eta',
-        'etd',
-        // end ROZANA
-    ];
-    public function awbs()
-    {
-        return $this->hasMany(\twa\smsautils\Models\Awb::class);
-    }
+    'scheduled_date',
+    'reference1',
+    'reference2',
+    'reference3',
+    'payment_reference1',
+    'payment_reference2',
+    'payment_reference3',
+    'added_services',
+    'transaction_id',
+    'billing_reference',
+    'currency',
+    'product_code',
+    'product_group',
+    'payment_type',
+    'payment_method',
+    'sender_address_id',
+    'receiver_address_id',
+    'client_id',
+    'customer_id',
+    'customs_payment_type',
+    'vat_payment_type',
+    'freight_payment_method',
+    'customs_payment_method',
+    'vat_payment_method',
+    'freight_payment_type',
+    'insurance',
+    'freight_payment_type',
+    'sla_delivery_date',
+    'dutiable',
+    'source',
+    'created_at',
+    // ROZANA
+    'eta',
+    'etd',
+    // end ROZANA
+  ];
+  public function awbs()
+  {
+    return $this->hasMany(\twa\smsautils\Models\Awb::class);
+  }
 
-      public function client()
-    {
-        return $this->belongsTo(\twa\smsautils\Models\Client::class);
-    }
-    
-    public function customer()
-    {
-        return $this->belongsTo(\twa\smsautils\Models\Contact::class);
-    }
- 
+  public function client()
+  {
+    return $this->belongsTo(\twa\smsautils\Models\Account::class);
+  }
+
+  public function customer()
+  {
+    return $this->belongsTo(\twa\smsautils\Models\Contact::class);
+  }
 }
