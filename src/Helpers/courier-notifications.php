809ec2c44af8c66cb9cd04ed2b7d49ed6b23b2c7
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-01-20 11:53:42 +0200

    Update courier-notifications.php

--- src/Helpers/courier-notifications.php
+++ src/Helpers/courier-notifications.php
@@ -170,7 +170,7 @@ if (!function_exists('notify_general_alert')) {
         $title_ar = "تنبيه";
         $message_en = "You have a new notification";
         $message_ar = "لديك إشعار جديد";
-        
+
         send_notification_helper(
             $title_en,
             $title_ar,
@@ -182,8 +182,9 @@ if (!function_exists('notify_general_alert')) {
     }
 }
 
-if(!function_exists('mission_transfer_alert')){
-    function mission_transfer_alert($courier_id, $data = null){
+if (!function_exists('mission_transfer_alert')) {
+    function mission_transfer_alert($courier_id, $data = null)
+    {
         $title_en = "Mission Transfer";
         $title_ar = "تحويل مهمة";
         $message_en = "You have a new mission transfer";
@@ -199,33 +200,35 @@ if(!function_exists('mission_transfer_alert')){
         );
     }
 }
-if(!function_exists('mission_transfer_request_received')){
-    function mission_transfer_request_received($courier_id, $data = null){
+if (!function_exists('mission_transfer_request_received')) {
+    function mission_transfer_request_received($courier_id, $data = null)
+    {
         $title_en = "Mission Transfer Request Received we will notify you when if it's approved";
         $title_ar = "ويتم ارسال طلب تحويل مهمة وسنخطرك عندما يتم الموافقة عليه";
         $message_en = "You have a new mission transfer request we will notify you when if it's approved";
         $message_ar = "لديك طلب تحويل مهمة جديد وسنخطرك عندما يتم الموافقة عليه";
-    
 
-    send_notification_helper(
-        $title_en,
-        $title_ar,
-        $message_en,
-        $message_ar,
-        ["condition" => ["user_id"], "value" => [$courier_id]],
-        $data
-    );
+
+        send_notification_helper(
+            $title_en,
+            $title_ar,
+            $message_en,
+            $message_ar,
+            ["condition" => ["user_id"], "value" => [$courier_id]],
+            $data
+        );
+    }
 }
-}
 
-if(!function_exists('mission_transfer_request_approved')){
+if (!function_exists('mission_transfer_request_approved')) {
 
-    function mission_transfer_request_approved($courier_id, $data = null){
+    function mission_transfer_request_approved($courier_id, $data = null)
+    {
         $title_en = "Mission Transfer Request Approved";
         $title_ar = "تم الموافقة على طلب تحويل مهمة";
         $message_en = "Your mission transfer request has been approved";
         $message_ar = "تم الموافقة على طلب تحويل مهمتك";
-    
+
 
 
         send_notification_helper(
@@ -295,56 +298,57 @@ if (!function_exists('assign_courier_to_runsheet')) {
     }
 }
 
-if(!function_exists('unassign_courier_from_pickup')){
-    function unassign_courier_from_pickup($courier_id, $pickup_id, $data = null){
+if (!function_exists('unassign_courier_from_pickup')) {
+    function unassign_courier_from_pickup($courier_id, $pickup_id, $data = null)
+    {
         $title_en = "Unassign Courier from Pickup" . $pickup_id;
         $title_ar = "تم إلغاء تعيين موظف للاستلام" . $pickup_id;
         $message_en = "You have been unassigned from the pickup" . $pickup_id;
         $message_ar = "تم إلغاء تعيينك من الاستلام" . $pickup_id;
-    
-    send_notification_helper(
-        $title_en,
-        $title_ar,
-        $message_en,
-        $message_ar,
-        ["condition" => ["user_id"], "value" => [$courier_id]],
-        $data
-    );
-  }
+
+        send_notification_helper(
+            $title_en,
+            $title_ar,
+            $message_en,
+            $message_ar,
+            ["condition" => ["user_id"], "value" => [$courier_id]],
+            $data
+        );
+    }
 }
-if(!function_exists('notify_courier_change_address')){
-    function notify_courier_change_address($courier_id , $awb, $data = null){
+if (!function_exists('notify_courier_change_address')) {
+    function notify_courier_change_address($courier_id, $awb, $data = null)
+    {
         $title_en = "Change Address Alert for shipment " . $awb;
         $title_ar = "تنبيه تغيير العنوان للشحنة " . $awb;
         $message_en = "You have a new address for shipment $awb";
         $message_ar = "لديك تغيير عنوان جديد للشحنة $awb";
-    
-    send_notification_helper(
-        $title_en,
-        $title_ar,  
-        $message_en,
-        $message_ar,
-        ["condition" => ["user_id"], "value" => [$courier_id]],
-        $data
-    );
-    }
-    if (!function_exists('notify_courier_change_route')) {
-        function notify_courier_change_route($courier_id, $awb, $data = null)
-        {
-            $title_en = "Change Route Alert for shipment " . $awb;
-            $title_ar = "تنبيه تغيير الرحلة للشحنة " . $awb;
-            $message_en = "You have a new route for shipment $awb";
-            $message_ar = "لديك تغيير رحلة جديد للشحنة $awb";
 
-            send_notification_helper(
-                $title_en,
-                $title_ar,
-                $message_en,
-                $message_ar,
-                ["condition" => ["user_id"], "value" => [$courier_id]],
-                $data
-            );
-        }
+        send_notification_helper(
+            $title_en,
+            $title_ar,
+            $message_en,
+            $message_ar,
+            ["condition" => ["user_id"], "value" => [$courier_id]],
+            $data
+        );
+    }
 }
+if (!function_exists('notify_courier_change_route')) {
+    function notify_courier_change_route($courier_id, $awb, $data = null)
+    {
+        $title_en = "Change Route Alert for shipment " . $awb;
+        $title_ar = "تنبيه تغيير الرحلة للشحنة " . $awb;
+        $message_en = "You have a new route for shipment $awb";
+        $message_ar = "لديك تغيير رحلة جديد للشحنة $awb";
+
+        send_notification_helper(
+            $title_en,
+            $title_ar,
+            $message_en,
+            $message_ar,
+            ["condition" => ["user_id"], "value" => [$courier_id]],
+            $data
+        );
     }
 }
