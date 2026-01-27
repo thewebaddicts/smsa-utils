e8a97f1af2f6f6f2b705a2c7bb8a2b2c907e230a
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-01-27 13:38:11 +0200

    Update awb-received-status.php

--- src/Helpers/awb-received-status.php
+++ src/Helpers/awb-received-status.php
@@ -13,19 +13,40 @@ if (!function_exists('update_awb_received_status')) {
     {
         $user = request()->user();
         $user_type = request()->input('user_type');
-$location = $location ?? 'operations';
+        $location = $location ?? 'operations';
         $awb = extract_awb_model_from_record($awb);
         if ($awb->origin_hub_id == $hub_id && $awb->origin_received_at == null) {
             $awb->status = AwbStatusEnum::ORIGIN_RECEIVED;
             $awb->origin_received_at = Carbon::now();
-            log_awb_activity(AwbStatusEnum::ORIGIN_RECEIVED, $awb->id,  $user->id ?? null, $user_type ?? null, 'Origin received from '.$location);
+            log_awb_activity(
+                AwbStatusEnum::ORIGIN_RECEIVED,
+                $awb->awb,
+                $awb->id,
+                $user->id ?? null,
+                $user_type ?? null,
+                'Origin received from ' . $location
+            );
         } elseif ($awb->origin_hub_id != $awb->destination_hub_id && $awb->origin_received_at  && $awb->destination_received_at == null) {
             $awb->status = AwbStatusEnum::DESTINATION_RECEIVED;
             $awb->destination_received_at = Carbon::now();
-            log_awb_activity(AwbStatusEnum::DESTINATION_RECEIVED, $awb->id, $user->id ?? null, $user_type ?? null, 'Destination received from '.$location);
+            log_awb_activity(
+                AwbStatusEnum::DESTINATION_RECEIVED,
+                $awb->awb,
+                $awb->id,
+                $user->id ?? null,
+                $user_type ?? null,
+                'Destination received from ' . $location
+            );
         } else {
             $awb->status = AwbStatusEnum::RECEIVED_OPERATION;
-            log_awb_activity(AwbStatusEnum::RECEIVED_OPERATION, $awb->id,  $user->id ?? null, $user_type ?? null, 'Received operation from '.$location);
+            log_awb_activity(
+                AwbStatusEnum::RECEIVED_OPERATION,
+                $awb->awb,
+                $awb->id,
+                $user->id ?? null,
+                $user_type ?? null,
+                'Received operation from ' . $location
+            );
         }
 
         if ($save) {
