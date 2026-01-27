78f9056f9c2663052fe8d56b48f556700cb58a6f
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-01-27 14:16:08 +0200

    Update awb-received-status.php

--- src/Helpers/awb-received-status.php
+++ src/Helpers/awb-received-status.php
@@ -16,12 +16,29 @@ if (!function_exists('update_awb_received_status')) {
         $location = $location ?? 'operations';
         $awb = extract_awb_model_from_record($awb);
 
+        // Debug log to understand which branch is taken
+        Log::info('update_awb_received_status:before', [
+            'awb_id' => $awb->id ?? null,
+            'awb_number' => $awb->awb ?? null,
+            'origin_hub_id' => $awb->origin_hub_id ?? null,
+            'destination_hub_id' => $awb->destination_hub_id ?? null,
+            'origin_received_at' => $awb->origin_received_at ?? null,
+            'destination_received_at' => $awb->destination_received_at ?? null,
+            'passed_hub_id' => $hub_id,
+            'location' => $location,
+        ]);
+
         // Origin receive: first time the AWB is received at its origin hub
         // Use empty() so we also treat "zero" dates as not received yet.
         if ($awb->origin_hub_id == $hub_id && empty($awb->origin_received_at)) {
 
             $awb->last_status = AwbStatusEnum::ORIGIN_RECEIVED;
             $awb->origin_received_at = Carbon::now();
+
+            Log::info('update_awb_received_status:origin_branch', [
+                'awb_id' => $awb->id ?? null,
+                'hub_id' => $hub_id,
+            ]);
 
             log_awb_activity(
                 AwbStatusEnum::ORIGIN_RECEIVED,
@@ -42,6 +59,11 @@ if (!function_exists('update_awb_received_status')) {
 
             $awb->last_status = AwbStatusEnum::DESTINATION_RECEIVED;
 
+            Log::info('update_awb_received_status:destination_branch', [
+                'awb_id' => $awb->id ?? null,
+                'hub_id' => $hub_id,
+            ]);
+
             $awb->destination_received_at = Carbon::now();
             log_awb_activity(
                 AwbStatusEnum::DESTINATION_RECEIVED,
@@ -54,6 +76,11 @@ if (!function_exists('update_awb_received_status')) {
         } else {
 
             $awb->last_status = AwbStatusEnum::RECEIVED_OPERATION;
+
+            Log::info('update_awb_received_status:operation_branch', [
+                'awb_id' => $awb->id ?? null,
+                'hub_id' => $hub_id,
+            ]);
 
             log_awb_activity(
                 AwbStatusEnum::RECEIVED_OPERATION,
