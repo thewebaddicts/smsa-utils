497bbffe85cdf84dca8d4c5210f9ba3e0811cfb2
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-01-27 13:39:30 +0200

    Update awb-received-status.php

--- src/Helpers/awb-received-status.php
+++ src/Helpers/awb-received-status.php
@@ -15,9 +15,12 @@ if (!function_exists('update_awb_received_status')) {
         $user_type = request()->input('user_type');
         $location = $location ?? 'operations';
         $awb = extract_awb_model_from_record($awb);
+
         if ($awb->origin_hub_id == $hub_id && $awb->origin_received_at == null) {
-            $awb->status = AwbStatusEnum::ORIGIN_RECEIVED;
+
+            $awb->last_status = AwbStatusEnum::ORIGIN_RECEIVED;
             $awb->origin_received_at = Carbon::now();
+
             log_awb_activity(
                 AwbStatusEnum::ORIGIN_RECEIVED,
                 $awb->awb,
@@ -26,8 +29,11 @@ if (!function_exists('update_awb_received_status')) {
                 $user_type ?? null,
                 'Origin received from ' . $location
             );
-        } elseif ($awb->origin_hub_id != $awb->destination_hub_id && $awb->origin_received_at  && $awb->destination_received_at == null) {
-            $awb->status = AwbStatusEnum::DESTINATION_RECEIVED;
+        } 
+        elseif ($awb->origin_hub_id != $awb->destination_hub_id && $awb->origin_received_at  && $awb->destination_received_at == null) {
+
+            $awb->last_status = AwbStatusEnum::DESTINATION_RECEIVED;
+
             $awb->destination_received_at = Carbon::now();
             log_awb_activity(
                 AwbStatusEnum::DESTINATION_RECEIVED,
@@ -38,7 +44,9 @@ if (!function_exists('update_awb_received_status')) {
                 'Destination received from ' . $location
             );
         } else {
-            $awb->status = AwbStatusEnum::RECEIVED_OPERATION;
+
+            $awb->last_status = AwbStatusEnum::RECEIVED_OPERATION;
+
             log_awb_activity(
                 AwbStatusEnum::RECEIVED_OPERATION,
                 $awb->awb,
