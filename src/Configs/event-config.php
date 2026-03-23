690de8a9c4972300be1d1778b69f07810420faac
nourhane-sarieddine <nourhane.sarieddine@thewebaddicts.com>
2026-03-23 12:55:52 +0200

    Update event-config.php

--- src/Configs/event-config.php
+++ src/Configs/event-config.php
@@ -1,4 +1,8 @@
 <?php
+
+use twa\smsautils\Classes\CreateClientPortalNotification;
+use twa\smsautils\Classes\HoldShipment;
+use twa\smsautils\Classes\RTS;
 use twa\smsautils\Classes\SendWP;
 use twa\smsautils\Classes\SendSMS;
 use twa\smsautils\Classes\SendOTP;
@@ -17,4 +21,7 @@ return [
     'd4e5f6g7-h8i9-4a01-b234-c5d6e7f8g901' => SendEmail::class,
 
     'e5f6g7h8-i9j0-4b12-c345-d6e7f8g9h012' => SendWebhook::class,
+    'f6g7h8i9-j0k1-4c23-d456-e7f8g9h0i123' => CreateClientPortalNotification::class,
+    'g7h8i9j0-k1l2-4d34-e567-f8g9h0i1j234' => HoldShipment::class,
+    'h8i9j0k1-l2m3-4e45-f678-g9h0i1j2k345' => RTS::class,
 ];
