3f65e30a66cf221a55dbec93014c55f6a2d6d0e9
nourhane-sarieddine <nourhane.sarieddine@thewebaddicts.com>
2026-03-23 15:38:39 +0200

    Update event-config.php

--- src/Configs/event-config.php
+++ src/Configs/event-config.php
@@ -12,11 +12,11 @@ return [
 
 
 
-    'a1b2c3d4-e5f6-4789-a012-b3c4d5e6f789' => SendWP::class,
+    // 'a1b2c3d4-e5f6-4789-a012-b3c4d5e6f789' => SendWP::class,
 
     'b2c3d4e5-f6a7-4890-b123-c4d5e6f7a890' => SendSMS::class,
 
-    'c3d4e5f6-g7h8-4901-b234-c5d6e7f8g901' => SendOTP::class,
+    // 'c3d4e5f6-g7h8-4901-b234-c5d6e7f8g901' => SendOTP::class,
 
     'd4e5f6g7-h8i9-4a01-b234-c5d6e7f8g901' => SendEmail::class,
 
