7aabccea2134ebb43a0ac9252a4f618896765a58
nourhane-sarieddine <nourhane.sarieddine@thewebaddicts.com>
2026-04-28 16:23:48 +0300

    Create ExceptionCase.php

---
+++ src/Classes/ExceptionCase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace twa\smsautils\Classes;
+
+use App\Contracts\EventHandlerInterface;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use twa\smsautils\Models\AccessToken;
+
+class ExceptionCase extends HandlerParent
+{
+    public function label(): string
+    {
+        return 'Exception Case';
+    }
+
+    public function payload(): array
+    {
+        return [
+            [
+                'column' => 'exception_category_id',
+                'label' => 'Exception Category',
+                'type' => 'select',
+                'required' => true,
+                'options' => \twa\smsautils\Models\ExceptionCategory::all()->pluck('label', 'id'),
+            ],
+            [
+                'column' => 'exception_trigger_reason_id',
+                'label' => 'Exception Trigger Reason',
+                'type' => 'select',
+                'required' => true,
+                'options' => \twa\smsautils\Models\ExceptionTriggerReason::all()->pluck('label', 'id'),
+            ]
+        ];
+    }
+
+    public function handle(array $variables, string|null $payload): bool
+    {
+
+        $array = [
+            'awb' => $variables['parent_awb'],
+            'exception_category_id' => $payload['exception_category_id'],
+            'exception_trigger_reason_id' => $payload['exception_trigger_reason_id'],
+            'comments' =>  null,
+            'files' => null,
+            'created_by_id' => 2,
+            'created_by_type' => 'workflow',
+        ];
+
+        try {
+            create_record_in_exception($array);
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+}
