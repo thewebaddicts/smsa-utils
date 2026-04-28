d8db4477aed89e4b84ef0b322dfc9b0dc0a1f59b
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-04-28 15:56:38 +0300

    Update AttributeForEnum.php

--- src/Enums/AttributeForEnum.php
+++ src/Enums/AttributeForEnum.php
@@ -7,21 +7,26 @@ enum AttributeForEnum: string
     case ADDRESS = 'address';
     case MAWB_MANIFEST = 'mawb_manifest';
     case SHIPMENT = 'shipment';
-public static function info($value)
-{ //label and value
-    return match ($value) {
-        self::ADDRESS => [
-            'label' => 'Address',
-            'value' => self::ADDRESS,
-        ],
-        self::MAWB_MANIFEST => [
-            'label' => 'MAWB Manifest',
-            'value' => self::MAWB_MANIFEST,
-        ],
-    self::SHIPMENT => [
-            'label' => 'Shipment',
-            'value' => self::SHIPMENT,
-        ],
-    };
+    case SHIPPER = 'shipper';
+    public static function info($value)
+    { //label and value
+        return match ($value) {
+            self::ADDRESS => [
+                'label' => 'Address',
+                'value' => self::ADDRESS,
+            ],
+            self::MAWB_MANIFEST => [
+                'label' => 'MAWB Manifest',
+                'value' => self::MAWB_MANIFEST,
+            ],
+            self::SHIPMENT => [
+                'label' => 'Shipment',
+                'value' => self::SHIPMENT,
+            ],
+            self::SHIPPER => [
+                'label' => 'Shipper',
+                'value' => self::SHIPPER,
+            ],
+        };
+    }
 }
-} 
