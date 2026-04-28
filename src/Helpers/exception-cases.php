302f2b34729c902554e7c418ba73b8a1296bf7db
nourhane-sarieddine <nourhane.sarieddine@thewebaddicts.com>
2026-04-28 18:30:41 +0300

    Update exception-cases.php

--- src/Helpers/exception-cases.php
+++ src/Helpers/exception-cases.php
@@ -3,6 +3,18 @@
 if (!function_exists('create_record_in_exception')) {
     function create_record_in_exception(array $payload)
     {
+
+
+        $awb_exception_exists = \twa\smsautils\Models\ExceptionCase::where('awb', $payload['awb'])
+            ->where('exception_category_id', $payload['exception_category_id'])
+            ->where('exception_trigger_reason_id', $payload['exception_trigger_reason_id'])
+            ->whereNotNull('resolved_at')->whereNull('deleted_at')->exists();
+
+        if ($awb_exception_exists) {
+            return false;
+        }
+
+
         $exceptionCase = new \twa\smsautils\Models\ExceptionCase();
         $exceptionCase->awb = $payload['awb'];
         $exceptionCase->exception_category_id = $payload['exception_category_id'];
