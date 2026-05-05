53233e53495d5e99d395c7b69841f9d29dc3a5a7
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-05-05 10:56:45 +0300

    up

--- src/Enums/DimensionUnit.php
+++ src/Enums/DimensionUnit.php
@@ -9,4 +9,15 @@ enum DimensionUnit: string
     case MM = 'mm';
     case IN = 'in';
     case FT = 'ft';
+
+    public function getRatioToCM(): float
+    {
+        return match($this) {
+            self::CM => 1,
+            self::M => 100,
+            self::MM => 0.1,
+            self::IN => 2.54,
+            self::FT => 30.48,
+        };
+    }
 } 
--- src/Enums/WeightUnit.php
+++ src/Enums/WeightUnit.php
@@ -8,4 +8,13 @@ enum WeightUnit: string
     case KG = 'kg';
     case LB = 'lb';
     case LBS = 'lbs';
+    
+    public function getRatioToKG(): float
+    {
+        return match($this) {
+            self::G => 1/1000,
+            self::KG => 1,
+            self::LB => 453.592/1000
+        };
+    }
 } 
