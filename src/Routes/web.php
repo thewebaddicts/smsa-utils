8f5e03bf8cd4d48f8be561f29177c4b15a73d561
nourhane-sarieddine <nourhane.sarieddine@thewebaddicts.com>
2026-05-28 15:09:20 +0300

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -13,6 +13,6 @@ Route::get('transaction-inventories/{transaction_inventory_id}/payment-receipt',
 
 
 Route::get('/awb/{awb}/view', [twa\smsautils\Http\Controllers\SmsaLabelController::class, 'viewLabel'])->name('awb.view');
-Route::get('/awb/{awb}/pdf', [SmsaLabelController::class, 'generatePdf'])->name('awb.pdf');
+// Route::get('/awb/{awb}/pdf', [SmsaLabelController::class, 'generatePdf'])->name('awb.pdf');
 
 Route::get('/run-migration-pack', [twa\smsautils\Http\Controllers\MigrationsController::class, 'run']);
