23412e20f470182f1ea8dc4d5f50d0a4c69b7603
Rozana-alhalabi <rosana.alhalabi@thewebaddicts.com>
2026-06-03 17:16:27 +0300

    Update OneSignalController.php

--- src/Http/Controllers/OneSignalController.php
+++ src/Http/Controllers/OneSignalController.php
@@ -2,9 +2,7 @@
 
 namespace twa\smsautils\Http\Controllers;
 
-
-use SoapClient;
-use SoapFault;
+use Illuminate\Support\Facades\Http;
 
 class OneSignalController
 {
@@ -83,25 +81,14 @@ class OneSignalController
                 }
 
             }
-        $fields = json_encode($fields);
-//        print("\nJSON sent:\n");
-//        print($fields);
-            $ch = \curl_init();
-            \curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-            \curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-                'Authorization: Basic '.$this->data['AuthKey']));
-            \curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            \curl_setopt($ch, CURLOPT_HEADER, FALSE);
-            \curl_setopt($ch, CURLOPT_POST, TRUE);
-            \curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-            \curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json; charset=utf-8',
+                'Authorization' => 'Basic ' . $this->data['AuthKey'],
+            ])
+                ->withOptions(['verify' => false])
+                ->post('https://onesignal.com/api/v1/notifications', $fields);
 
-            $response = \curl_exec($ch);
-            \curl_close($ch);
-
-            $response = json_decode($response,1);
-
-            return $response;
+            return $response->json() ?? [];
 
         }
 }
